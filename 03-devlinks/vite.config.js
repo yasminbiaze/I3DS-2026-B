edb7984bbe322b1edc5022eb8933589c0b99b5ba
yasminbiaze <yasbiazee@gmail.com>
2026-02-12 11:29:43 -0300

    feat: reestruturar o componente App e adicionar o componente Perfil

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, //Muda a porta padão git para a padrão react(3000)
+    open: true, //Abre o navegador automaticamente
+    host: true, //Permite acesso externo, IP do local(opcional, dependendo do seu caso de uso)
+},
 })
