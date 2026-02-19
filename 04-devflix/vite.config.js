b099650a0abda78db5f6dbb019ddb3072d2a2e60
yasminbiaze <yasbiazee@gmail.com>
2026-02-19 09:06:38 -0300

    feat: adicionar configuração do servidor no Vite para porta 3000

--- 04-devflix/vite.config.js
+++ 04-devflix/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, //Muda a porta padrão vite(5173) para a padrão react(3000)
+    open: true, //Abre o navegador automaticamente
+    host: true, //Permite que tenha acesso via IP na rede local
+  },
 })
