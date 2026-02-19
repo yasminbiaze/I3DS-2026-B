6afdb0afc892b4cf0e89deaa629c330f140a1e0d
yasminbiaze <yasbiazee@gmail.com>
2026-02-19 11:21:01 -0300

    feat: adicionar componente Rodape e integrar ao App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
+
 const App = () => {
-  return <div> App </div>
- 
-}
+  return (
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming DEVFLIX com letras vermelhas 
+    e fundo preto, destacando a plataforma de conteúdo de vídeos sob demanda."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
 
+      <Rodape>Yasmin De Biaze</Rodape>
+    </div>
+  );
+};
+
+export default App;
---
+++ 04-devflix/src/components/Rodape/Rodape.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Rodape.module.css";
+
+const Rodape = ({ children }) => {
+  return (
+    <footer>
+      <p>
+        Feito com 💕por <a href="https://github.com">{children}</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
